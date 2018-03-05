b540fe831532110176dffc2ff5a2b2d7ac7a049c
Luka Kotur <looko95@gmail.com>
2018-03-05 20:28:37 +0100

    Added Edit functionality

--- src/main.js
+++ src/main.js
@@ -7,13 +7,19 @@ import 'vuetify/dist/vuetify.min.css'
 import {store} from './store'
 import DateFilter from './filters/date'
 import AlertComponent from './components/shared/Alert.vue'
-
+import EditMeetupDetailsDialog from './components/Meetup/Edit/EditMeetupDetailsDialog'
+import EditMeetupDateDialog from './components/Meetup/Edit/EditMeetupDateDialog'
+import EditMeetupTimeDialog from './components/Meetup/Edit/EditMeetupTimeDialog'
+  ''
 Vue.use(Vuetify);
 
 Vue.config.productionTip = false;
 
 Vue.filter('date', DateFilter);
 Vue.component('app-alert', AlertComponent);
+Vue.component('app-edit-meetup-details-dialog', EditMeetupDetailsDialog);
+Vue.component('app-edit-meetup-date-dialog', EditMeetupDateDialog);
+Vue.component('app-edit-meetup-time-dialog', EditMeetupTimeDialog);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
